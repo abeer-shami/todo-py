7752d6c456e5c6b038fc4e40fa59e4595ff04e62
abeer-shami <abeer.shami887@gmail.com>
2018-02-11 15:21:56 +0000

    setting the db and trying to connect it but still not working

--- app.py
+++ app.py
@@ -1,17 +1,60 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, flash, redirect, url_for, request, logging
+from wtforms import Form, StringField, TextAreaField, validators
+from flask_mysqldb import MySQL
 
 app = Flask (__name__)
 
+# init MYSQL
+mysql = MySQL(app)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'tododb'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_PORT'] = '3306'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/add', methods=['POST'])
+@app.route('/add')
+def task():
+    return render_template('add.html')
+
+# Task Form Class
+class TaskForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=55)])
+    description = TextAreaField('Description', [validators.Length(min=5)])
+
+# Add Article
+@app.route('/add', methods=['GET', 'POST'])
 def add():
-    return redirect(url_for('index'))
+    form = TaskForm(request.form)
+    if request.method == 'GET':
+        return render_template('add.html')
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        description = form.description.data
 
+        # Create Cursor
+        cur = mysql.connection.cursor()
 
+        # Execute
+        cur.execute("INSERT INTO todotask(name, description) VALUES(%s, %s)",(name, description))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        flash('the task created ', 'success')
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form)
 if __name__ == '__main__':
-    app.run(debug = True)    
+    app.run(debug = True)
